190bf4eb2898f89027abdbd65de622453e880388
Felipe Sousa <sousa.felipe@outlook.com>
2025-11-20 20:16:22 -0300

    feat: foi adicionado um método estático à classe 'TextUtils', chamado 'convertTo', que tem a capacidade de converter uma string base64 para qualquer outro formato

--- src/utils/TextUtils.ts
+++ src/utils/TextUtils.ts
@@ -1,7 +1,36 @@
 
 export class TextUtils {
   
+  /**
+   * Remove caracteres "anormais" de uma string.
+   * 
+   * @param text Uma string contendo caracteres "anormais".
+   * @returns Uma string com apenas letras de 'a' a 'z' (minúsculas ou maiúsculas), dígitos de '0'
+   *          a '9' e o caractere de sublinhado _.
+   */
   static normalize(text: string): string {
     return text.replace(/[^a-z0-9_]/gi, '').trim();
+  }
+
+  /**
+   * Converte uma string de um formato para outro definido pelo parâmetro **format**.
+   * 
+   * @param text Uma string codificada (geralmente em algum formato não legível por seres humanos).
+   * @param format O formato para o qual se deseja converter. (UTF-8 é o padrão)
+   * @returns Uma string convertida para o formato definido pelo parâmetro **format**.
+   */
+  static convertTo(text: string, format: string = 'utf-8'): string {
+    const chars = atob(text);
+    if (!chars.length) {
+      return '';
+    }
+
+    const bytes = new Uint8Array(chars.length);
+    for (let i = 0; i < chars.length; i++) {
+      bytes[i] = chars.charCodeAt(i);   
+    }
+
+    const decoder = new TextDecoder(format);
+    return decoder.decode(bytes);
   }
 }
