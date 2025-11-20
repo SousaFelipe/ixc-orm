383b5e8608371470180d414445288377a5acac7d
Felipe Sousa <sousa.felipe@outlook.com>
2025-11-20 13:25:50 -0300

    test: correção da mensagem esperada pelo teste

--- tests/recursos/radusuarios_25452.test.ts
+++ tests/recursos/radusuarios_25452.test.ts
@@ -3,7 +3,7 @@ import { Recurso } from '../../src/index';
 
 describe('Recurso: limparMAC', () => {
 
-  it('deve retornar a mensagem "MAC removido com sucesso..."', async () => {
+  it('deve retornar a mensagem "MAC removido com sucesso"...', async () => {
 
     const id_login = 8554;
     const response = await Recurso.limparMAC({ id_login });
