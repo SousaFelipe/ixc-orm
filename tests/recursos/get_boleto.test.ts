3f147b77b2b4cf1f4990f24004ce6156c91a43de
Felipe Sousa <sousa.felipe@outlook.com>
2025-11-20 20:17:48 -0300

    test: o recurso 'Recurso.getArquivoBoleto()' foi testado e funcionou de acordo como esperado.

--- tests/recursos/get_boleto.test.ts
+++ tests/recursos/get_boleto.test.ts
@@ -1,13 +1,16 @@
 import { Recurso } from '../../src/index';
+import { TextUtils } from '../../src/utils/TextUtils';
 
 
 describe('Recurso: getArquivoBoleto', () => {
 
-  it('deve retornar um arquivo PDF de um boleto...', async () => {
+  it('deve retornar o conteúdo de um arquivo PDF de um boleto...', async () => {
     const id_fatura = 344003;
-    const pdf = await Recurso.getArquivoBoleto({ id_fatura });
-  
-    expect(pdf).toContain('PDF-1.7')
+    const response = await Recurso.getArquivoBoleto({ id_fatura });
+
+    const utf8Content = TextUtils.convertTo(response.content());
+    
+    expect(utf8Content).toContain('PDF-1.')
   });
 
 });
