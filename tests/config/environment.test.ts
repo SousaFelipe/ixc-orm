b3d50176c548b3f5759352ad43c1a001ff26b9d3
Felipe Sousa <sousa.felipe@outlook.com>
2025-12-24 01:34:32 -0300

    test: correção do método invocano na instância da classe Environment

--- tests/config/environment.test.ts
+++ tests/config/environment.test.ts
@@ -4,12 +4,12 @@ import { Environment } from '../../src/index';
 describe('Variáveis de ambiente', () => {
 
   it('deve encontrar a variável IXC_ACCESS_TOKEN', async () => {
-    const token = Environment.getInstance().getToken();
+    const token = Environment.loadInstance().getToken();
     expect(token).toEqual('mesmo token que está configurado no arquivo .env ou no docker-compose.yaml');
   });
 
   it('deve encontrar a variável IXC_SERVER_DOMAIN', async () => {
-    const domain = Environment.getInstance().getDomain();
+    const domain = Environment.loadInstance().getDomain();
     expect(domain).toEqual('mesmo domínio que está configurado no arquivo .env ou no docker-compose.yaml');
   });
 
